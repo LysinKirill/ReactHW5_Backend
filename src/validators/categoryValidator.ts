e3df3c7fcb953283e4fc42f1d61e1dbffa29ac33
klysin <klysin@ozon.ru>
2025-02-25 18:26:44 +0300

    do not call middleware next if validation failed

--- src/validators/categoryValidator.ts
+++ src/validators/categoryValidator.ts
@@ -13,6 +13,7 @@ export const validate = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         res.status(400).json({ errors: errors.array() });
+        return;
     }
     next();
 };
--- src/validators/productValidator.ts
+++ src/validators/productValidator.ts
@@ -21,6 +21,7 @@ export const validate = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         res.status(400).json({ errors: errors.array() });
+        return;
     }
     next();
 };
